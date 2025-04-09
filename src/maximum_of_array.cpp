6d8a65052c77cde4812a9e51396e4aec9ecbf46f
VahramKtratsyan <vahram.ktratsyan@gmail.com>
2025-04-09 13:37:45 +0400

    Source files of the project

---
+++ src/maximum_of_array.cpp
@@ -0,0 +1,8 @@
+int max(int array[], int size)
+{
+    int res  = array[0];
+    for (int i = 0; i < size - 1; ++i)
+        if (res < array[i + 1])
+            res = array[i + 1];   
+    return res;
+}
