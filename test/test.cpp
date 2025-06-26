854fff38dc1931b0ed8b07836eedfe0e4e34c007
VahramKtratsyan <vahram.ktratsyan@gmail.com>
2025-06-26 22:54:08 +0400

    Tests to check project accuracy

--- test/test.cpp
+++ test/test.cpp
@@ -1,12 +1,12 @@
 #include <gtest/gtest.h>
-#include "maximum_of_array.hpp"
+#include "../header/maximum_of_array.hpp"
 
 TEST(maximumOfArrayTest, ArrayOf0_Returns0)
 {
    constexpr int size = 1;
    int res = 0;
    int array[size] = {res}; 
-   ASSERT_EQ(max(array, size), 0);
+   EXPECT_EQ(max(array, size), 0);
 }
 
 TEST(maximumOfArrayTest, ArrayOf1_Returns1)
@@ -14,7 +14,7 @@ TEST(maximumOfArrayTest, ArrayOf1_Returns1)
    constexpr int size = 1;
    int res = 1;
    int array[size] = {res}; 
-   ASSERT_EQ(max(array, size), res);
+   EXPECT_EQ(max(array, size), res);
 }
 
 TEST(maximumOfArrayTest, ArrayOf2_Returns2)
@@ -22,7 +22,7 @@ TEST(maximumOfArrayTest, ArrayOf2_Returns2)
    constexpr int size = 1;
    int res = 2;
    int array[size] = {res}; 
-   ASSERT_EQ(max(array, size), res);
+   EXPECT_EQ(max(array, size), res);
 }
 
 TEST(maximumOfArrayTest, ArrayOf1Element_ReturnsTheElement)
@@ -30,7 +30,7 @@ TEST(maximumOfArrayTest, ArrayOf1Element_ReturnsTheElement)
    constexpr int size = 1;
    int res = std::rand();
    int array[size] = {res}; 
-   ASSERT_EQ(max(array, size), res);
+   EXPECT_EQ(max(array, size), res);
 }
 
 TEST(maximumOfArrayTest, ArrayOf2SameElements_ReturnsTheSameElement)
@@ -40,7 +40,7 @@ TEST(maximumOfArrayTest, ArrayOf2SameElements_ReturnsTheSameElement)
    int array[size];
    for (int i = 0; i < size; ++i)
       array[i] = res;
-   ASSERT_EQ(max(array, size), res);
+   EXPECT_EQ(max(array, size), res);
 }
 
 TEST(maximumOfArrayTest, ArrayOf50SameElements_ReturnsTheSameElement)
@@ -50,7 +50,7 @@ TEST(maximumOfArrayTest, ArrayOf50SameElements_ReturnsTheSameElement)
    int array[size]; 
    for (int i = 0; i < size; ++i)
       array[i] = res;
-   ASSERT_EQ(max(array, size), res);
+   EXPECT_EQ(max(array, size), res);
 }
 
 TEST(maximumOfArrayTest, ArrayOf0_1_Returns1)
@@ -58,7 +58,7 @@ TEST(maximumOfArrayTest, ArrayOf0_1_Returns1)
    constexpr int size = 2;
    int res = 1;
    int array[size] = {0, res}; 
-   ASSERT_EQ(max(array, size), res);
+   EXPECT_EQ(max(array, size), res);
 }
 
 TEST(maximumOfArrayTest, ArrayOf1_0_Returns1)
@@ -66,7 +66,7 @@ TEST(maximumOfArrayTest, ArrayOf1_0_Returns1)
    constexpr int size = 2;
    int res = 1;
    int array[size] = {res, 0}; 
-   ASSERT_EQ(max(array, size), res);
+   EXPECT_EQ(max(array, size), res);
 }
 
 TEST(maximumOfArrayTest, ArrayOf1_0_0_Returns1)
@@ -74,7 +74,7 @@ TEST(maximumOfArrayTest, ArrayOf1_0_0_Returns1)
    constexpr int size = 3;
    int res = 1;
    int array[size] = {res, 0, 0}; 
-   ASSERT_EQ(max(array, size), res);
+   EXPECT_EQ(max(array, size), res);
 }
 
 TEST(maximumOfArrayTest, ArrayOf0_1_0_Returns1)
@@ -82,7 +82,7 @@ TEST(maximumOfArrayTest, ArrayOf0_1_0_Returns1)
    constexpr int size = 3;
    int res = 1;
    int array[size] = {0, res, 0}; 
-   ASSERT_EQ(max(array, size), res);
+   EXPECT_EQ(max(array, size), res);
 }
 
 TEST(maximumOfArrayTest, ArrayOf0_0_1_Returns1)
@@ -90,7 +90,7 @@ TEST(maximumOfArrayTest, ArrayOf0_0_1_Returns1)
    constexpr int size = 3;
    int res = 1;
    int array[size] = {0, 0, 1}; 
-   ASSERT_EQ(max(array, size), res);
+   EXPECT_EQ(max(array, size), res);
 }
 
 TEST(maximumOfArrayTest, ArrayOf0_2_1_1_Returns2)
@@ -98,7 +98,7 @@ TEST(maximumOfArrayTest, ArrayOf0_2_1_1_Returns2)
    constexpr int size = 4;
    int res = 2;
    int array[size] = {0, 2, 1,  1}; 
-   ASSERT_EQ(max(array, size), res);
+   EXPECT_EQ(max(array, size), res);
 }
 
 TEST(maximumOfArrayTest, ArrayOf25DifferentElements_ReturnsMax_968)
@@ -106,5 +106,5 @@ TEST(maximumOfArrayTest, ArrayOf25DifferentElements_ReturnsMax_968)
    constexpr int size = 25;
    int res{968};
    int array[size] = {28, 744, 20, 523, -741, 688, 646, -227, 392, 249, 968, 727, 583, 740, 4, 395, -49, 147, 14, 545, 220, 770, 780, 656, 0}; 
-   ASSERT_EQ(max(array, size), res);
+   EXPECT_EQ(max(array, size), res);
 }
